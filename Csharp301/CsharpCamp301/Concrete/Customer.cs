297fe1f63057e9ea89c675c7de65df14cee69865
BerzanBaran <berzanbaran.1808@gmail.com>
2025-10-26 22:30:19 +0300

    301 moduls added

---
+++ Csharp301/CsharpCamp301/Concrete/Customer.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CsharpCamp301.Concrete
+{
+    public class Customer
+    {
+
+        public int customerID { get; set; }
+
+        public string customerName { get; set; }
+
+
+        public string customerSurname { get; set; }
+        public string customerDistrict { get; set; }
+        public string customerCity { get; set; }
+
+ 
+        public List<Order> ordesr { get; set; }
+
+
+    }
+}
---
+++ Csharp301/CsharpCamp301/Concrete/Order.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CsharpCamp301.Concrete
+{
+    public class Order
+    {
+
+        public int OrderID { get; set; }
+
+
+        public int ProductID { get; set; }
+
+        public virtual Product Product { get; set; }
+
+        public int Quantity { get; set; }
+
+        public decimal UnitPrice { get; set; }
+        public decimal totalPrice { get; set; }
+
+        public int customerID { get; set; }
+        public virtual Customer customer { get; set; }
+
+
+
+
+    }
+}
